46b29e02e804ae68a3086f71fde2cb3c52fc245e
Eviive <albert.vaillon21@gmail.com>
2023-03-08 20:37:47 +0100

    feat: added header profile dropdown

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,7 +16,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class SecurityController extends AbstractController
 {
-	#[Route('/register', name: 'app_register')]
+	#[Route('/register', name: 'app_security_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, BlogAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
@@ -48,7 +48,7 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'app_security_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
@@ -66,7 +66,7 @@ class SecurityController extends AbstractController
 		]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'app_security_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');

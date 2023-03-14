944e86290d55a9d015a2f438ea8b80e72b2e1049
Eviive <albert.vaillon21@gmail.com>
2023-03-14 13:25:05 +0100

    feat: created some basic pages

--- src/Controller/User/CategoryController.php
+++ src/Controller/User/CategoryController.php
@@ -10,6 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
+    #[Route('/', name: 'app_home_category', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('pages/user/category/index.html.twig', [
+            'categories' => $categoryRepository->findAllNotEmpty(),
+        ]);
+    }
+
     public function getPopularCategories(CategoryRepository $categoryRepository): Response
     {
         return $this->render('components/_nav.html.twig', [
--- src/Controller/User/PostController.php
+++ src/Controller/User/PostController.php
@@ -2,11 +2,43 @@
 
 namespace App\Controller\User;
 
+use App\Repository\PostRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/post')]
 class PostController extends AbstractController
 {
+    /**
+     * @throws NonUniqueResultException
+     */
+    #[Route('/', name: 'app_home_index')]
+    public function index(PostRepository $postRepository): Response
+    {
+        $featured = $postRepository->findFeaturedPost();
 
+        $posts = $featured ? $postRepository->findAllExcept($featured->getId()) : [];
+
+        return $this->render('pages/user/post/index.html.twig', [
+            'featured' => $featured,
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/post', name: 'app_home_post')]
+    public function post(Request $request, PostRepository $postRepository, PaginatorInterface $paginator): Response
+    {
+        $pagination = $paginator->paginate(
+            $postRepository->findPagination(),
+            $request->request->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/user/post/post.html.twig', [
+            'pagination' => $pagination,
+        ]);
+    }
 }

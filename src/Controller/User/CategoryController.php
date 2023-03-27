f9dfbb92e3f3694a8a65f32ae313e89f4cc057a9
Eviive <albert.vaillon21@gmail.com>
2023-03-27 09:36:09 +0200

    feat: display a category's posts using its id

--- src/Controller/User/CategoryController.php
+++ src/Controller/User/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\User;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
-    #[Route('/', name: 'app_home_category', methods: ['GET'])]
+    #[Route('', name: 'app_home_category', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository): Response
     {
         return $this->render('pages/user/category/index.html.twig', [
             'categories' => $categoryRepository->findAllNotEmpty(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_home_category_show', methods: ['GET'])]
+    public function show(Category $category): Response
+    {
+        return $this->render('pages/user/category/show.html.twig', [
+            'category' => $category,
         ]);
     }
 

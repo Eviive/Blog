44e6aba218558b1ddaf9df0633e01db821a8cc53
Eviive <albert.vaillon21@gmail.com>
2023-04-01 15:10:25 +0200

    feat: added a flash component using Bootstrap Toasts

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -1,3 +1,28 @@
 import '../scss/app.scss';
-import 'bootstrap';
+import { Toast } from 'bootstrap';
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+(() => {
+    for (const el of document.querySelectorAll<HTMLDivElement>('div.toast')) {
+        const toast = new Toast(el, {
+            autohide: false
+        });
+
+        let minutes = 0;
+        const interval = setInterval(() => {
+            minutes++;
+
+            const time = el.querySelector<HTMLElement>('small.time');
+
+            if (time) {
+                time.textContent = minutes == 0 ? 'Now' : `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+            }
+        }, 60000);
+
+        el.addEventListener('hidden.bs.toast', () => {
+            clearInterval(interval);
+        });
+
+        toast.show();
+    }
+})();
